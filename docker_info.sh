2facba92a0c0ffb124a0b347659e5450dc7e30cf
silascript <cansaiji@gmail.com>
2022-06-24 18:41:04 +0800

    添加查询docker ip及端口占用信息的sh脚本

---
+++ docker_info.sh
@@ -0,0 +1,11 @@
+# 查询 Docker 容器 ip 占用情况
+
+# 查询 IPv4Address 主要用于 自定义网桥下容器 IP的查询
+function docker_ipv4_allcontainers(){
+	docker inspect -f '{{.Name}}: {{range .NetworkSettings.Networks}}{{.IPAMConfig.IPv4Address}}{{end}}' $(docker ps -aq)
+}
+
+# 查询 Docker 所有容器已指派的端口号
+function docker_ports_allcontainers(){
+	docker inspect --format='{{.Name}}: {{range $conf := .HostConfig.PortBindings}}{{(index $conf 0).HostPort}} {{end}}' $(docker ps -aq)
+}
